dccd20b7cd40621640e5b5c19b0f9510790b1fff
Beutin Julian <julian.beutin@outlook.fr>
2024-05-06 22:37:11 +0200

    :sparkles: add toolbar

--- front/erp/src/app/app.module.ts
+++ front/erp/src/app/app.module.ts
@@ -5,16 +5,18 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {AuthModule} from "./auth/auth.module";
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {ToolbarComponent} from "./shared/toolbar/toolbar.component";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AuthModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AuthModule,
+        ToolbarComponent
+    ],
   providers: [
     provideAnimationsAsync()
   ],
