04f4ddb622c521d27b81ea895da83ea1f6a74218
Julian <julian.beutin@viacesi.fr>
2024-05-12 15:36:29 +0200

    :sparkles: rajout de trucs en plus

--- front/erp/src/app/auth/auth.module.ts
+++ front/erp/src/app/auth/auth.module.ts
@@ -3,12 +3,13 @@ import {CommonModule} from '@angular/common';
 import {RegisterComponent} from "./register/register.component";
 import {LoginComponent} from "./login/login.component";
 import {AuthRoutingModule} from "./auth-routing.module";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
   imports: [
-    CommonModule, AuthRoutingModule
+    CommonModule, AuthRoutingModule, ReactiveFormsModule
   ]
 })
 export class AuthModule {
--- front/erp/src/app/auth/register/register.component.ts
+++ front/erp/src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {AuthService} from "../../core/service/auth.service";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,15 @@ import {AuthService} from "../../core/service/auth.service";
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+
+  userForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirm: new FormControl(''),
+  });
 
   constructor(private authService: AuthService) {
   }
