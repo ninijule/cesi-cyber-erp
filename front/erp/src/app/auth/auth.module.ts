427eb24d91967c0d2f268f7790ecb921b3e524e7
Beutin Julian <julian.beutin@outlook.fr>
2024-05-15 23:36:28 +0200

    :sparkles: page de register

--- front/erp/src/app/auth/auth.module.ts
+++ front/erp/src/app/auth/auth.module.ts
@@ -4,12 +4,15 @@ import {RegisterComponent} from "./register/register.component";
 import {LoginComponent} from "./login/login.component";
 import {AuthRoutingModule} from "./auth-routing.module";
 import {ReactiveFormsModule} from "@angular/forms";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
   imports: [
-    CommonModule, AuthRoutingModule, ReactiveFormsModule
+    CommonModule, AuthRoutingModule, ReactiveFormsModule, MatFormField, MatLabel, MatInput, MatButton, MatError
   ]
 })
 export class AuthModule {
--- front/erp/src/app/auth/register/register.component.ts
+++ front/erp/src/app/auth/register/register.component.ts
@@ -1,6 +1,27 @@
 import {Component} from '@angular/core';
 import {AuthService} from "../../core/service/auth.service";
-import {FormControl, FormGroup} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
+
+
+export const matchFieldsValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const email = control.get('email');
+  const emailConfirm = control.get('emailConfirm');
+  const password = control.get('password');
+  const passwordConfirm = control.get('passwordConfirm');
+
+  const errors: ValidationErrors = {};
+
+  if (email && emailConfirm && email.value !== emailConfirm.value) {
+    errors['emailMismatch'] = true;
+  }
+
+  if (password && passwordConfirm && password.value !== passwordConfirm.value) {
+    errors['passwordMismatch'] = true;
+  }
+
+  return Object.keys(errors).length ? errors : null;
+};
 
 @Component({
   selector: 'app-register',
@@ -11,19 +32,35 @@ export class RegisterComponent {
 
 
   userForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    passwordConfirm: new FormControl(''),
-  });
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    emailConfirm: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    passwordConfirm: new FormControl('', [Validators.required]),
+  }, {validators: matchFieldsValidator});
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private _snackBar: MatSnackBar) {
+  }
+
+  get emailMismatch(): boolean {
+    return this.userForm.hasError('emailMismatch');
+  }
+
+  get passwordMismatch(): boolean {
+    return this.userForm.hasError('passwordMismatch');
   }
 
 
-  onRegister(): void {
-    this.authService.registerUser('', '').subscribe(response => console.log(response));
+  onSubmit(): void {
+    if (this.userForm.valid) {
+      this.authService.registerUser('', '').subscribe(response => console.log(response));
+    } else {
+      this._snackBar.open('Please fill all fields of the form.', '', {
+        duration: 1500,
+      });
+    }
   }
 
 }
