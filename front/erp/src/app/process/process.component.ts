21e155069f4844bce0e8686f1d7d9b3375d38c28
Beutin Julian <julian.beutin@outlook.fr>
2024-05-16 21:10:30 +0200

    :sparkles: ajout process

---
+++ front/erp/src/app/process/process.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-process',
+  templateUrl: './process.component.html',
+  styleUrl: './process.component.scss'
+})
+export class ProcessComponent {
+
+}
--- front/erp/src/app/process/process.module.ts
+++ front/erp/src/app/process/process.module.ts
@@ -5,10 +5,11 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
+import {ProcessComponent} from "./process.component";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProcessComponent],
   imports: [
     CommonModule, ProcessRoutingModule, ReactiveFormsModule, MatFormField, MatLabel, MatInput, MatButton, MatError
   ]
