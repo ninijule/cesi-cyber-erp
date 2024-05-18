32ceeaa0bdf555286f2426df8666b2b417c4affe
Beutin Julian <julian.beutin@outlook.fr>
2024-05-18 20:46:19 +0200

    :sparkles: affichages des processus

--- front/erp/src/app/process/process.module.ts
+++ front/erp/src/app/process/process.module.ts
@@ -6,12 +6,14 @@ import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
 import {ProcessComponent} from "./process.component";
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
 
 
 @NgModule({
   declarations: [ProcessComponent],
   imports: [
-    CommonModule, ProcessRoutingModule, ReactiveFormsModule, MatFormField, MatLabel, MatInput, MatButton, MatError
+    CommonModule, ProcessRoutingModule, ReactiveFormsModule, MatFormField, MatLabel, MatInput, MatButton, MatError, MatGridList, MatGridTile, MatCard, MatCardHeader, MatCardContent, MatCardTitle, MatCardSubtitle
   ]
 })
 export class ProcessModule {
