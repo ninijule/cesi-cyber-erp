23759a7411ed4123261fc7bc79567a76a095c1a0
Beutin Julian <julian.beutin@outlook.fr>
2024-05-22 22:48:23 +0200

    :sparkles: rajout de la page de home

--- front/src/app/auth/auth.module.ts
+++ front/src/app/auth/auth.module.ts
@@ -7,10 +7,11 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
+import {AuthComponent} from "./auth.component";
 
 
 @NgModule({
-  declarations: [RegisterComponent, LoginComponent],
+  declarations: [RegisterComponent, LoginComponent, AuthComponent],
   imports: [
     CommonModule, AuthRoutingModule, ReactiveFormsModule, MatFormField, MatLabel, MatInput, MatButton, MatError
   ]
