fbc4753e251ec7033eb141e03a7041ffb10ed194
Beutin Julian <julian.beutin@outlook.fr>
2024-05-22 21:56:28 +0200

    :sparkles: env pour prod et dev

--- back/config.ts
+++ back/config.ts
@@ -3,14 +3,13 @@ import fs from "node:fs";
 import {configDotenv} from "dotenv";
 
 
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env';
+const envFile = '.env';
 const envPath = path.resolve(__dirname, '../', envFile);
 
 if (fs.existsSync(envPath)) {
     configDotenv({path: envPath});
 } else {
-    console.error(`Le fichier d'environnement ${envFile} n'a pas été trouvé`);
-    process.exit(1); // Arrêter le processus si le fichier est manquant
+    console.error(`Attention le fichier d'environnement ${envFile} n'a pas été trouvé`);
 }
 console.log('Start application in mode : ' + process.env.NODE_ENV);
 
