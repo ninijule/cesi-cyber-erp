cf62a6cfac3d102468b178b73399c018ed8e1b5a
Beutin Julian <julian.beutin@outlook.fr>
2024-08-01 00:33:55 +0200

    :sparkles: rajout de seedeers à la création de la base

--- back/src/database/seeders/users.sql
+++ back/src/database/seeders/users.sql
@@ -1,19 +1,22 @@
-CREATE TABLE users
+CREATE TABLE IF NOT EXISTS users
 (
     id        SERIAL PRIMARY KEY,
-    firstName VARCHAR(50)  NOT NULL,
-    lastName  VARCHAR(50)  NOT NULL,
+    "firstName" VARCHAR(50)  NOT NULL,
+    "lastName"  VARCHAR(50)  NOT NULL,
     email     VARCHAR(100) NOT NULL,
     password  VARCHAR(100) NOT NULL,
     is_admin  BOOLEAN      NOT NULL DEFAULT FALSE,
-    createdAt TIMESTAMPTZ,
-    updatedAt TIMESTAMPTZ,
+    "createdAt" TIMESTAMPTZ,
+    "updatedAt" TIMESTAMPTZ,
     salary    NUMERIC
 );
 
 
-INSERT INTO users (id, firstName, lastName, email, password, is_admin, createdAt, updatedAt, salary)
+INSERT INTO users (id, "firstName", "lastName", email, password, is_admin, "createdAt", "updatedAt", salary)
 VALUES (1, 'Bob', ' Page', 'admin@cesi.fr', '5e761456804e455ece24468b010043d8', true, now(), now(), 55000);
 
-INSERT INTO users (id, firstName, lastName, email, password, is_admin, createdAt, updatedAt, salary)
+INSERT INTO users (id, "firstName", "lastName", email, password, is_admin, "createdAt", "updatedAt", salary)
 VALUES (2, 'Mohamad', ' Elfalou', 'melfalou@cesi.fr', '5e761456804e455ece24468b010043d8', false, now(), now(), 65000);
+
+INSERT INTO users (id, "firstName", "lastName", email, password, is_admin, "createdAt", "updatedAt", salary)
+VALUES (3, 'Julian', ' Beutin', 'julian.beutin@viacesi.fr', '5e761456804e455ece24468b010043d8', false, now(), now(), 65000);
