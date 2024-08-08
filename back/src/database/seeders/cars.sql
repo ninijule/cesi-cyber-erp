85b3a5f6536f315e76ca6eca3abc15bd73bc9811
Beutin Julian <julian.beutin@outlook.fr>
2024-08-09 00:39:18 +0200

    :sparkles: seeders

--- back/src/database/seeders/cars.sql
+++ back/src/database/seeders/cars.sql
@@ -13,3 +13,6 @@ CREATE TABLE IF NOT EXISTS cars
 INSERT INTO cars (id, name, description, img, price, is_active, "createdAt")
 VALUES (1, 'Porsche 911', ' La Porsche 911 est une voiture de sport haut de gamme fabriquée par le constructeur automobile allemand Porsche. La première génération est commercialisée en 1964, intégralement conçue par la firme de Stuttgart.', '/assets/img/porsche.jpg', 180000, true, now());
 
+
+INSERT INTO cars (id, name, description, img, price, is_active, "createdAt")
+VALUES (2, 'Ferrari', '', '/assets/img/ferrari.jpg', 180000, true, now());
